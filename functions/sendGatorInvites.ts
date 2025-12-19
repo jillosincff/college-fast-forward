1cbaec1fd8a692372dfd07a4ae95e64331af0e5a
Jill Osinoff <josinoff@gmail.com>
2025-12-19 20:25:35 +0000

    File changes

--- functions/sendGatorInvites.ts
+++ functions/sendGatorInvites.ts
@@ -127,7 +127,7 @@ Deno.serve(async (req) => {
                     const msg = {
                         to: email,
                         from: {
-                            email: 'hello@collegefastforward.com',
+                            email: 'support@collegefastforward.com',
                             name: 'College Fast Forward'
                         },
                         subject: subject,
