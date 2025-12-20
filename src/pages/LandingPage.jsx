4bbc7b298f302ef4f58a4ba0420b2ea5bf9812ad
Jill Osinoff <josinoff@gmail.com>
2025-12-20 06:31:36 +0000

    File changes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -243,16 +243,7 @@ export default function LandingPage() {
             backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='1'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`
           }} />
           
-          <div className="relative z-10 max-w-5xl mx-auto px-4 py-16 md:py-24 text-center">
-            {/* Logo - Transparent background */}
-            <div className="mb-8">
-              <img 
-                src={TRANSPARENT_LOGO}
-                alt="College Fast Forward"
-                className="h-24 md:h-32 mx-auto"
-              />
-            </div>
-
+          <div className="relative z-10 max-w-5xl mx-auto px-4 py-12 md:py-20 text-center">
             {/* Main Headline */}
             <motion.h1 
               initial={{ opacity: 0, y: 20 }}
