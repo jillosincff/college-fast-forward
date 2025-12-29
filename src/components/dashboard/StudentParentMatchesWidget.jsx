e9fa9d68a92abe02fdd6bb11e7026e69e9ec8ec8
Jill Osinoff <josinoff@gmail.com>
2025-12-29 02:27:37 +0000

    File changes

--- src/components/dashboard/StudentParentMatchesWidget.jsx
+++ src/components/dashboard/StudentParentMatchesWidget.jsx
@@ -387,7 +387,7 @@ export default function StudentParentMatchesWidget({ user, matches = [], onRefre
               onClick={() => navigate('MyMatches')}
               className="text-blue-600 border-blue-300 hover:bg-blue-50"
             >
-              View All {filteredMatches.length} Matches
+              View All My Matches
               <ArrowRight className="w-4 h-4 ml-2" />
             </Button>
           </div>
