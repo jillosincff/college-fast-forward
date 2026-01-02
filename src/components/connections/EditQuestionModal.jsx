96531a327064e33b5ccb459f486bb012a3e77998
Jill Osinoff <josinoff@gmail.com>
2026-01-02 04:34:55 +0000

    File changes

--- src/components/connections/EditQuestionModal.jsx
+++ src/components/connections/EditQuestionModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { JobRequest } from '@/entities/JobRequest';
 import { HelpRequest } from '@/entities/HelpRequest';
+import { updateJobRequest } from '@/functions/updateJobRequest';
 import {
   Dialog,
   DialogContent,
