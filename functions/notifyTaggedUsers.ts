a50826da6c8a4452567c0a3006717fdb04be40a6
Jill Osinoff <josinoff@gmail.com>
2026-02-25 21:46:57 +0000

    File changes

--- functions/notifyTaggedUsers.ts
+++ functions/notifyTaggedUsers.ts
@@ -1,5 +1,6 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 
+// Notify users who were @mentioned in answers
 Deno.serve(async (req) => {
   try {
     const base44 = createClientFromRequest(req);
