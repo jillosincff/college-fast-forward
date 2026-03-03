74a79aaf2745212e8bca7987b2cab9e4b4f3e408
Jill Osinoff <josinoff@gmail.com>
2026-03-03 15:02:50 +0000

    File changes

--- functions/fastTrackProAgent.ts
+++ functions/fastTrackProAgent.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 // Detect if user is asking about alumni/connections at a company
 function detectAlumniQuery(message) {
