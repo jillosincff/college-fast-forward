9da223b0137ccf12ec5ef02ae03d60e2c87cd5c6
Jill Osinoff <josinoff@gmail.com>
2026-03-03 01:48:20 +0000

    File changes

--- functions/createFamilyAndCheckout.ts
+++ functions/createFamilyAndCheckout.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 import Stripe from 'npm:stripe@14.21.0';
 
 /**
--- functions/stripeWebhook.ts
+++ functions/stripeWebhook.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'npm:@base44/sdk@0.8.4';
+import { createClient } from 'npm:@base44/sdk@0.8.6';
 import Stripe from 'npm:stripe@14.21.0';
 
 const base44 = createClient({
