cbbf0d54fc6efea57dd4323ef9557f4cd4183608
Jill Osinoff <josinoff@gmail.com>
2026-03-04 07:54:46 +0000

    File changes

--- functions/handleMessageReply.ts
+++ functions/handleMessageReply.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.5.0';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 // Function to be called when a poster sends a message reply
 // Updates application status and cancels reminders
