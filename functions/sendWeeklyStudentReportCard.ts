5024f18b57a777150e80229fcbbf7f1ca6a614d9
Jill Osinoff <josinoff@gmail.com>
2026-03-04 07:27:07 +0000

    File changes

--- functions/sendWeeklyStudentReportCard.ts
+++ functions/sendWeeklyStudentReportCard.ts
@@ -1,6 +1,7 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 Deno.serve(async (req) => {
+  console.log('[sendWeeklyStudentReportCard] Starting...');
   try {
     const base44 = createClientFromRequest(req);
     const user = await base44.auth.me();
