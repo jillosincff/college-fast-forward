853c563bd67f2577843aeb666d6a6a579e40f35b
Jill Osinoff <josinoff@gmail.com>
2026-03-06 23:38:33 +0000

    File changes

--- functions/registerUser.ts
+++ functions/registerUser.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'npm:@base44/sdk@0.8.4';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 import bcrypt from 'npm:bcryptjs@2.4.3';
 
 // Centralized function to send the verification email
