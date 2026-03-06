bc674a0396579fa2f730c7c38b4466eda527d3dd
Jill Osinoff <josinoff@gmail.com>
2026-03-06 04:02:42 +0000

    File changes

--- functions/createFamilyAndCheckout.ts
+++ functions/createFamilyAndCheckout.ts
@@ -21,7 +21,7 @@ const FOUNDING_LIMIT = 1000;
 const STRIPE_PRICES = {
   cff_monthly:    'price_1SUJ2g873TV7WMcTBYvmzGYU',  // $9/month
   fastiq_monthly: 'price_1SUJ7I873TV7WMcT1plkAZpz',  // $29/month
-  fastiq_annual:  'price_1T7pOU873TV7WMcTbbBXguCb',     // $249/year
+  fastiq_annual:  'price_1T7pQp873TV7WMcTdp7SsboC',     // $249/year
 };
 
 const TRIAL_DAYS = 7;
