e3639ba91a108e6e68d82e87e929799d253f00d9
Jill Osinoff <josinoff@gmail.com>
2026-03-07 03:43:55 +0000

    File changes

--- functions/getFoundingStats.ts
+++ functions/getFoundingStats.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 const FOUNDING_LIMIT = 1000;
 const EARLY_ADOPTER_LIMIT = 5000;
