8f1e604b8c80e811ba0e101d081dc70cc064fbb9
Jill Osinoff <josinoff@gmail.com>
2026-03-17 20:39:56 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -21,7 +21,7 @@ import AppErrorBoundary from './components/common/AppErrorBoundary';
 import { base44 } from '@/api/base44Client';
 import logger from './components/utils/logger';
 import { Message } from '@/entities/Message';
-// PayItForwardNotification import kept for notification bell
+import { PayItForwardNotification } from '@/entities/PayItForwardNotification';
 import NotificationBell from './components/notifications/NotificationBell';
 import { perfMonitor, reportWebVitals } from './components/utils/performanceMonitor';
 import { errorReporter } from './components/utils/errorReporter';
