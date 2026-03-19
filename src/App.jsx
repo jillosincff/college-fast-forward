811921882905964831b74870d9636bac4f47d2a5
Jill Osinoff <josinoff@gmail.com>
2026-03-19 18:51:00 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import FreeTierDashboard from '@/pages/FreeTierDashboard';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
@@ -60,6 +61,7 @@ const AuthenticatedApp = () => {
           }
         />
       ))}
+      <Route path="/FreeTierDashboard" element={<FreeTierDashboard />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
